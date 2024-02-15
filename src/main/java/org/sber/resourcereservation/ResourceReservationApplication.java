bc690098e6b3bdcf579e10bee141ad7145ff6fbd
Sapog87 <molotov9500@gmail.com>
2024-02-15 14:38:54 +0300

    Initial commit

---
+++ src/main/java/org/sber/resourcereservation/ResourceReservationApplication.java
@@ -0,0 +1,13 @@
+package org.sber.resourcereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sber/resourcereservation/ResourceReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sber.resourcereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
